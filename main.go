a6b1ee6716ec8314b98036d8c47829e05dead160
Mohsin Raza <99578958+Mohsinraza27@users.noreply.github.com>
2024-08-21 13:20:55 +0500

    Update main.go & It's Final Push

--- main.go
+++ main.go
@@ -16,22 +16,27 @@ func skillsPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
+	// Render the about html page from static folder
 	http.ServeFile(w, r, "static/about.html")
 }
 
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
+	// Render the contact html page from static folder
 	http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
+	// Serve static files (CSS, JS, images, etc.) directly from the "static" folder
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/home", homePage)
+	// Serve specific routes
+	http.HandleFunc("/", homePage)          // Serve home page at the root URL
 	http.HandleFunc("/skills", skillsPage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
 
+	// Start the server
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatal(err)
